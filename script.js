c97d67bfb25d81d486876424b09d0b37d9e9792b
João Vitor <joaovitor55670@gmail.com>
2022-08-03 19:29:58 -0300

    n sei o numero do commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let firstli = document.getElementsByTagName("li")[0]
+let firstli1 = document.getElementsByTagName("li")[1]
+let firstli2 = document.getElementsByTagName("li")[2]
+let firstli3 = document.getElementsByTagName("li")[3]
+let firstli4 = document.getElementsByTagName("li")[4]
+let firstli5 = document.getElementsByTagName("li")[5]
+let firstli6 = document.getElementsByTagName("li")[6]
+let firstli7 = document.getElementsByTagName("li")[7]
+let firstli8 = document.getElementsByTagName("li")[8]
+let firstli9 = document.getElementsByTagName("li")[9]
+
+
+
+let valor = ""
+
+firstli.addEventListener("click", function(){
+    
+    console.log(numbers[1])
+})
+
+let teclas = [firstli, firstli1, firstli2, firstli3, firstli4, firstli5, firstli6, firstli7, firstli8, firstli9]
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
