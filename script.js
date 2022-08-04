92e82c7f73144060d42a2619ae5b9d4c430c0a9e
João Vitor <joaovitor55670@gmail.com>
2022-08-04 15:46:17 -0300

    semi-final commit

--- script.js
+++ script.js
@@ -1,22 +1,71 @@
-let botoesn = document.getElementsByTagName("li")[0]
-let bottom2 = document.getElementsByTagName("li")[1]
-let a = 0
+let section = document.getElementById("sec1")
 let input = document.getElementById("ipt")
+let submit = document.getElementById("submit")
+let nulo = document.getElementById("nulo")
 
-
-
-function teste(){
-  input.value =  botoesn.value
-  
-  console.log(input.value)
-}
-
-function teste2(){
-    input.value += bottom2.value
+function numbers(e) {
+    input.value += num[e.target.id]
 }
 
 
+let num = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9
+]
+
+let names = [
+    "Ciro Gomes",
+    "Luis Inácio 'LULA' da Silva",
+    "Jair Bolsonaro",
+    "Obrigado por usar a urna eletrônica!",
+
+]
+
+function enviar() {
+
+    let img = document.getElementsByTagName("img")[0]
+    let img2 = document.getElementsByTagName("img")[1]
+    let h2 = document.getElementById("result")
+    let p = document.getElementsByTagName("p")[0]
+
+    img2.setAttribute("src", "./images/github.png")
+    p.innerHTML = names[3]
+
+    if (input.value == 00) {
+        console.log("aa")
+    }
 
 
-botoesn.addEventListener("click", teste)
-bottom2.addEventListener("click", teste2)
+    if (input.value == 12) {
+
+        img.setAttribute("src", "./images/ciro.jpg")
+        h2.innerHTML = names[0]
+
+    }
+
+    if (input.value == 13) {
+        img.setAttribute("src", "./images/lula.jpg")
+        h2.innerHTML = names[1]
+
+    }
+
+    if (input.value == 17) {
+        img.setAttribute("src", "./images/bolsonaro.jpg")
+        h2.innerHTML = names[2]
+    }
+}
+
+
+
+
+section.addEventListener("click", numbers)
+submit.addEventListener("click", enviar)
+
